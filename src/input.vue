b23f473d577c4cb7e3d6e7bdd21507cdb06682b8
mamba <1612786059@qq.com>
2019-04-04 00:29:56 +0800

    input组件

---
+++ src/input.vue
@@ -0,0 +1,104 @@
+<template>
+  <div
+    class="wrapper"
+    :class="{error}"
+  >
+    <input
+      :value="value"
+      type="text"
+      :disabled="disabled"
+      :readonly="readonly"
+    >
+    <template v-if="error">
+      <icon
+        name="error"
+        class="icon-error"
+      ></icon>
+      <span class="errorMessage">{{error}}</span>
+    </template>
+  </div>
+
+</template>
+<script>
+import Icon from "./icon.vue";
+
+export default {
+  components: { Icon },
+  name: "lunzi-input",
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+// html {
+//         --button-height: 32px;
+//         --font-size: 14px;
+//         --button-bg: white;
+//         --button-active-bg: #eee;
+//         --border-radius: 4px;
+//         --color: #333;
+//         --border-color: #999;
+//         --border-color-hover: #666;
+//     }
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #f1453d;
+.wrapper {
+  display: inline-flex;
+  align-items: center;
+  font-size: $font-size;
+  display: inline-block;
+  > :not(:last-child){
+      margin-right: .5em;
+  }
+  > input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+    font-size: inherit;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &[disabled],
+    &[readonly] {
+      border: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+  }
+  &.error {
+    > input {
+      border-color: $red;
+    }
+  }
+  .icon-error {
+    fill: $red;
+  }
+  .errorMessage {
+    color: red;
+  }
+}
+</style>
