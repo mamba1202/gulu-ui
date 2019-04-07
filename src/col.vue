da4d787bdaeaf1a228397710bea5e5f391f01d4c
mamba <1612786059@qq.com>
2019-04-07 13:02:11 +0800

    基本实现row和col的功能

--- src/col.vue
+++ src/col.vue
@@ -2,8 +2,11 @@
   <div
     class="col"
     :class="[`col-${span}`, offest && `offest-${offest}`]"
+    :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}"
   >
-    <slot></slot>
+  <div style="border: 1px solid green; height: 100px;">
+ <slot></slot>
+  </div> 
   </div>
 </template>
 <script>
@@ -15,16 +18,18 @@ export default {
     },
     offest: {
       type: [Number, String]
-    }
   }
-};
+},
+data(){
+    return{
+        gutter: 0
+    }
+}
+}
 </script>
 <style scoped lang="scss">
 .col {
-  height: 100px;
-  background: grey;
   width: 50%;
-  border: 1px solid red;
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
--- src/row.vue
+++ src/row.vue
@@ -1,15 +1,28 @@
 <template>
-    <div class="row">
-        <slot></slot>
-    </div>
+  <div
+    class="row"
+    :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px', }"
+  >
+    <slot></slot>
+  </div>
 </template>
 <script>
 export default {
-    
+  name: "lunzi-row",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  mounted(){
+    this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+    })
+}
 }
 </script>
 <style scoped lang="scss">
-.row{
-    display: flex;
+.row {
+  display: flex;
 }
 </style>
