51cc38b7a9b56441d2bc0f1b2c5a081123df83f2
mamba <1612786059@qq.com>
2019-04-08 14:15:28 +0800

    添加简单动画

--- src/layout.vue
+++ src/layout.vue
@@ -32,7 +32,6 @@ export default {
   flex-grow: 1;
   display: flex;
   flex-direction: column;
-  border: 1px solid red;
   &.hasSider {
     flex-direction: row;
   }
--- src/sider.vue
+++ src/sider.vue
@@ -1,17 +1,41 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="fade">
+    <div
+      class="sider"
+      v-if="visible"
+    >
+      <slot></slot>
+      <button @click="visible=false">close</button>
+    </div>
+    <transition>
 </template>
 
 <script>
 export default {
-  name:'lunzi-sider'
+  name: "lunzi-sider",
+  data() {
+    return {
+      visible: true
+    };
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.sider{
-    
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.fade-enter-active,
+.fade-enter-active {
+  transition: all 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  margin-left: -200px;
 }
 </style>
