304804542290b1d2c51076d9cd6c200417dfaa20
mamba <1612786059@qq.com>
2019-04-08 18:44:14 +0800

    添加toast和plugin

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import Header from './header.vue'
 import Content from './content.vue'
 import Footer from './footer.vue'
 import Sider from './sider.vue'
-
+import plugin from './plugin.js'
 
 Vue.component('g-button', Button)  //全局的g-button组件
 Vue.component('g-icon', Icon)
@@ -20,10 +20,10 @@ Vue.component('g-row', Row)
 Vue.component('g-col', Col)
 Vue.component('g-layout', Layout)
 Vue.component('g-header', Header)
-Vue.component('g-content',Content)
-Vue.component('g-footer',Footer)
-Vue.component('g-sider',Sider)
-
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
+Vue.use(plugin)
 new Vue({
     el: '#app',
     data: {
@@ -32,123 +32,13 @@ new Vue({
         loading3: false,
         message: 'hi',
     },
-    methods:{
-inputChange(e){
-    console.log(e.target.value)
-}
+    created() {
+     
+    },
+    methods: {
+        showToast() {
+            this.$toast('我是message')
+        }
     }
 })
-
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-
-const expect = chai.expect
-
-//单元测试
-{
-    const Constructor = Vue.extend(Button)  //把button组件变成一个构造函数
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })  //通过构造函数搞出一个实例
-    button.$mount('#test')   //把实例挂载到test
-    let useElement = button.$el.querySelector('use')
-    console.log(useElement)
-    //expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-settings')
-    button.$el.remove() //删除元素
-    button.$destroy() //删除对象
-}
-{
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true
-        }
-    })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
-    console.log(useElement)
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-    button.$el.remove()
-    button.$destroy()
-}
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
-    console.log(svg)
-    //let order = window.getComputedStyle(svg).order
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq("1")
-    button.$el.remove()
-    button.$destroy()
-}
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings',
-            iconPosition: 'right'
-        }
-    })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
-    console.log(svg)
-    //let order = window.getComputedStyle(svg).order
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq("2")
-    button.$el.remove()
-    button.$destroy()
-} {
-    const Constructor = Vue.extend(Button)
-    const gButton = new Constructor({
-        propsData: {
-            icon: 'settings',
-        }
-    })
-    gButton.$mount()
-    let spy = chai.spy(function () {
-        let button = gButton.$el
-        button.click()  //调用
-        expect(spy).to.have.been.called() //已经被调用了
-    })
-}
-// catch(error){
-//   window.errors=[error]
-//}finally{
-//   window.errors && window.errors.forEach((error)=>{
-//      console.error(error.message)
-//   })
-//}
-
-// {   //mock
-//     const Constructor = Vue.extend(Button)
-//     const gButton = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//         }
-//     })
-//     gButton.$mount()
-//     gButton.$on('click', function () {
-//         expect(1).to.eq(1)
-//     })
-//     //希望这个函数被执行
-//     let button =gButton.$el
-//     button.click()
-// }
 
---
+++ src/toast.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "lunzi-toast"
+};
+</script>
+<style lang="scss" scoped>
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.75);
+$box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+.toast {
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: $font-size;
+  color: white;
+  height: $toast-height;
+  line-height: 1.8;
+  display: flex;
+  align-items: center;
+  background: $toast-bg;
+  box-shadow: $box-shadow;
+  padding: 0 16px;
+  border-radius: 4px;
+}
+</style>
