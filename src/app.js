8da5139b4b25747c17d4d9c6524b1e7bc688a3bc
mamba <1612786059@qq.com>
2019-04-10 23:18:37 +0800

    可以设置默认selected

--- src/app.js
+++ src/app.js
@@ -53,7 +53,8 @@ new Vue({
        // loading2: true,
        // loading3: false,
        // message: 'hi',
-        selectedTab: 'sports'
+       // selectedTab: 'sports'
+       selectedTab: '2'
     },
     created() {
         // this.$toast('hello',{
