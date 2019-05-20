d6c31479e611b4742a19327bf53ec4d65770ab1a
mamba <1612786059@qq.com>
2019-05-20 11:07:17 +0800

    添加button-group组件

---
+++ docs/.vuepress/components/button-group.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <g-button-group icon="left" icon-position="left">上一页</g-button-group>
+        <g-button-group icon="right" icon-position="right">下一页</g-button-group>
+    </div>
+</template>
+<script>
+import Button from '../../../src/button'
+import ButtonGroup from '../../../src/button-group'
+export default {
+    components:{
+        'g-button':Button,
+        'g-button-group': ButtonGroup
+    }
+    
+}
+</script>
+
