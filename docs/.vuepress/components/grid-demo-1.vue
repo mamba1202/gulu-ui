1aaa972ead751bde5e4519d703bd862b6f494545
mamba <1612786059@qq.com>
2019-05-20 20:10:42 +0800

    调整预览代码/添加TIP提示

--- docs/.vuepress/components/grid-demo-1.vue
+++ docs/.vuepress/components/grid-demo-1.vue
@@ -91,7 +91,6 @@
     <p>
       <strong>代码</strong>
     </p>
-    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <style scoped>
@@ -104,7 +103,7 @@
   .demoCol {
     height: 50px;
     border: 1px solid #ccc;
-    background: #eee;
+    background: #7cb8ff;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -115,97 +114,5 @@
   import GCol from '../../../src/col'
   export default {
     components: {GRow, GCol},
-    data () {
-      return {
-        content: `
-          * {
-            box-sizing: border-box;
-          }
-          <g-row class="demoRow">
-            <g-col span="8">
-              <div class="demoCol">8</div>
-            </g-col>
-            <g-col span="8">
-              <div class="demoCol">8</div>
-            </g-col>
-            <g-col span="8">
-              <div class="demoCol">8</div>
-            </g-col>
-          </g-row>
-          <g-row class="demoRow">
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-          </g-row>
-          <g-row class="demoRow">
-            <g-col span="4">
-              <div class="demoCol">4</div>
-            </g-col>
-            <g-col span="4">
-              <div class="demoCol">4</div>
-            </g-col>
-            <g-col span="4">
-              <div class="demoCol">4</div>
-            </g-col>
-            <g-col span="4">
-              <div class="demoCol">4</div>
-            </g-col>
-            <g-col span="4">
-              <div class="demoCol">4</div>
-            </g-col>
-            <g-col span="4">
-              <div class="demoCol">4</div>
-            </g-col>
-          </g-row>
-          <g-row class="demoRow">
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-            <g-col span="2">
-              <div class="demoCol">2</div>
-            </g-col>
-          </g-row>
-      `.replace(/^ {8}/gm, '').trim()
-      }
-    }
   }
 </script>
--- docs/.vuepress/components/grid-demo-2.vue
+++ docs/.vuepress/components/grid-demo-2.vue
@@ -1,6 +1,5 @@
 <template>
   <div style="padding-top: 16px;">
-    <h2>设置 gutter</h2>
     <p>
       <strong>预览</strong>
     </p>
@@ -39,7 +38,6 @@
     <p>
       <strong>代码</strong>
     </p>
-    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <style scoped>
@@ -52,7 +50,7 @@
 .demoCol {
   height: 50px;
   border: 1px solid #ccc;
-  background: #eee;
+  background: #7cb8ff;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -63,41 +61,5 @@ import GRow from "../../../src/row";
 import GCol from "../../../src/col";
 export default {
   components: { GRow, GCol },
-  data() {
-    return {
-      content: `
-          * {
-            box-sizing: border-box;
-          }
-          <g-row class="demoRow" gutter="10">
-            <g-col span="8">
-              <div class="demoCol">8</div>
-            </g-col>
-            <g-col span="8">
-              <div class="demoCol">8</div>
-            </g-col>
-            <g-col span="8">
-              <div class="demoCol">8</div>
-            </g-col>
-          </g-row>
-          <g-row class="demoRow" gutter="10">
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-            <g-col span="6">
-              <div class="demoCol">6</div>
-            </g-col>
-          </g-row>
-      `
-        .replace(/^ {8}/gm, "")
-        .trim()
-    };
-  }
 };
 </script>
