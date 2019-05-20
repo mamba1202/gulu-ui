2786a692007a36392b70cf866c04c5513f52809a
mamba <1612786059@qq.com>
2019-05-20 11:08:18 +0800

    添加更多按钮

--- docs/.vuepress/components/button-demo-1.vue
+++ docs/.vuepress/components/button-demo-1.vue
@@ -1,18 +1,14 @@
 <template>
   <div style="padding-top: 16px;">
-    <h2>简单用法</h2>
     <p>
       <strong>预览</strong>
     </p>
     <g-button>默认按钮</g-button>
-    <g-button icon="settings">默认按钮</g-button>
-    <g-button :loading="true">默认按钮</g-button>
+    <g-button icon="settings">设置</g-button>
+    <g-button icon="download">点赞</g-button>
+    <g-button icon="thumbs-up">下载</g-button>
+    <g-button :loading="true">登陆中</g-button>
     <g-button disabled>默认按钮</g-button>
-
-    <p>
-      <strong>代码</strong>
-    </p>
-    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <script>
@@ -21,18 +17,6 @@ import Button from "../../../src/button";
 export default {
   components: {
     "g-button": Button
-  },
-  data() {
-    return {
-      content: `
-          <g-button>默认按钮</g-button>
-          <g-button icon="settings">默认按钮</g-button>
-          <g-button :loading="true">默认按钮</g-button>
-          <g-button disabled>默认按钮</g-button> 
-      `
-        .replace(/^ {8}/gm, "")
-        .trim()
-    };
   }
 };
 </script>
