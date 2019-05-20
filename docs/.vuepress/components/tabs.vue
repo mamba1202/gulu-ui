2f17a0ef0a25cee1adf972b7cf256bece3088b85
mamba <1612786059@qq.com>
2019-05-20 20:18:28 +0800

    将预览代码移至对应md中

--- docs/.vuepress/components/tabs.vue
+++ docs/.vuepress/components/tabs.vue
@@ -1,6 +1,5 @@
 <template>
   <div style="padding-top: 16px;">
-    <h2>简单用法</h2>
     <p>
       <strong>预览</strong>
     </p>
@@ -14,11 +13,6 @@
         <g-tabs-pane name="2">content 2</g-tabs-pane>
       </g-tabs-body>
     </g-tabs>
-
-    <p>
-      <strong>代码</strong>
-    </p>
-    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <script>
@@ -29,25 +23,5 @@
   import GTabsPane from '../../../src/tabs-pane'
   export default {
     components: {GTabs, GTabsBody, GTabsHead, GTabsItem, GTabsPane},
-    data () {
-      return {
-        selected: '1',
-        content: `
-          data:{
-            selected: '1'
-          }
-          <g-tabs :selected="selected">
-            <g-tabs-head>
-              <g-tabs-item name="1">1</g-tabs-item>
-              <g-tabs-item name="2">2</g-tabs-item>
-            </g-tabs-head>
-            <g-tabs-body>
-              <g-tabs-pane name="1">content 1</g-tabs-pane>
-              <g-tabs-pane name="2">content 2</g-tabs-pane>
-            </g-tabs-body>
-          </g-tabs>
-      `.replace(/^ {8}/gm, '').trim()
-      }
-    }
   }
 </script>
