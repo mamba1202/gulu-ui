2b1f0e9f5e119ec7201a3511d06573613192ef30
mamba <1612786059@qq.com>
2019-05-20 20:16:32 +0800

    将预览代码移至对应md中

--- docs/.vuepress/components/layout-demo-3.vue
+++ docs/.vuepress/components/layout-demo-3.vue
@@ -1,12 +1,11 @@
 <template>
   <div style="padding-top: 16px;">
-    <h2>布局</h2>
     <p>
       <strong>预览</strong>
     </p>
 
     <g-layout style="color: white; overflow:hidden; margin-bottom:50px;">
-      <g-sider style=" background:#ddd; width:200px; color: black;">
+      <g-sider style=" background:#7cb8ff; width:200px; color: black;">
         sider
       </g-sider>
       <g-layout>
@@ -21,10 +20,6 @@
         </g-footer>
       </g-layout>
     </g-layout>
-    <p>
-      <strong>代码</strong>
-    </p>
-    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <style scoped>
@@ -39,28 +34,6 @@
   import GContent from '../../../src/content'
   import GSider from '../../../src/sider'
   export default {
-    components: {GLayout, GHeader, GFooter, GContent, GSider},
-    data () {
-      return {
-        content: `
-          <g-layout style="color: white; overflow:hidden; margin-bottom:50px;">
-            <g-sider style=" background:#ddd; width:200px; color: black;">
-              sider
-            </g-sider>
-            <g-layout>
-              <g-header style="height: 50px; background:lightskyblue;">
-                header
-              </g-header>
-              <g-content style="height: 100px; background:deepskyblue;">
-                content
-              </g-content>
-              <g-footer style="height: 50px; background:lightskyblue;">
-                footer
-              </g-footer>
-            </g-layout>
-          </g-layout>
-      `.replace(/^ {8}/gm, '').trim()
-      }
-    }
+    components: {GLayout, GHeader, GFooter, GContent, GSider}
   }
 </script>
