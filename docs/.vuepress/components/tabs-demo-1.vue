c0909b72c9a90bb66d84151088ed7bbce922b434
mamba <1612786059@qq.com>
2019-05-23 17:49:40 +0800

    up弹出bug

---
+++ docs/.vuepress/components/tabs-demo-1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="padding-top: 16px;">
+    <p>
+      <strong>预览</strong>
+    </p>
+    <g-tabs :selected="selected">
+      <g-tabs-head>
+        <g-tabs-item name="1">1</g-tabs-item>
+        <g-tabs-item name="2">2</g-tabs-item>
+      </g-tabs-head>
+      <g-tabs-body>
+        <g-tabs-pane name="1">content 1</g-tabs-pane>
+        <g-tabs-pane name="2">content 2</g-tabs-pane>
+      </g-tabs-body>
+    </g-tabs>
+  </div>
+</template>
+<script>
+  import GTabs from '../../../src/tabs'
+  import GTabsBody from '../../../src/tabs-body'
+  import GTabsHead from '../../../src/tabs-head'
+  import GTabsItem from '../../../src/tabs-item'
+  import GTabsPane from '../../../src/tabs-pane'
+  export default {
+    components: {GTabs, GTabsBody, GTabsHead, GTabsItem, GTabsPane},
+     data () {
+      return {
+        selected: '1',
+      }
+  }
+  }
+</script>
