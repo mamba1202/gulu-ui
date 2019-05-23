77521efd97c9d860e6a0429c4bf5e191b425234b
mamba <1612786059@qq.com>
2019-05-23 17:31:36 +0800

    添加collapse组件

---
+++ docs/.vuepress/components/collapse-demo-1.vue
@@ -0,0 +1,28 @@
+<template>
+    <div style="height: 209px; padding-bottom: 40px">
+        <p><strong>预览</strong></p>
+        <g-collapse :selected.sync="selectedTab">
+            <g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
+            <g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
+            <g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
+        </g-collapse>
+    </div>
+</template>
+
+<script>
+    import Collapse from '../../../src/collapse'
+    import CollapseItem from '../../../src/collapse-item'
+    export default {
+        components: {
+            'g-collapse': Collapse,
+            'g-collapse-item': CollapseItem
+        },
+        data() {
+            return {
+                selectedTab: ['1', '2', '3']
+            }
+        }
+    }
+</script>
+<style>
+</style>
