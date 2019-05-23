9f77e1e47467e73246bb40b99ba42ba3159b7b4e
mamba <1612786059@qq.com>
2019-05-23 17:35:56 +0800

    添加popover组件

--- docs/.vuepress/components/popover-demo-1.vue
+++ docs/.vuepress/components/popover-demo-1.vue
@@ -29,11 +29,6 @@
         弹出内容
       </template>
     </g-popover>
-
-    <p>
-      <strong>代码</strong>
-    </p>
-    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <style>
@@ -45,36 +40,6 @@
   import GPopover from '../../../src/popover'
   export default {
     components: {GButton, GPopover},
-    data () {
-      return {
-        content: `
-          <g-popover>
-            <g-button>上方弹出</g-button>
-            <template slot="content">
-              弹出内容
-            </template>
-          </g-popover>
-          <g-popover position="bottom">
-            <g-button>下方弹出</g-button>
-            <template slot="content">
-              弹出内容
-            </template>
-          </g-popover>
-          <g-popover position="left">
-            <g-button>左边弹出</g-button>
-            <template slot="content">
-              弹出内容
-            </template>
-          </g-popover>
-          <g-popover position="right">
-            <g-button>右边弹出</g-button>
-            <template slot="content">
-              弹出内容
-            </template>
-          </g-popover>
-      `.replace(/^ {8}/gm, '').trim()
-      }
-    },
     methods: {
       onClickButton () {
         this.$toast('<strong style="color:red;">加粗的提示</strong>', {
