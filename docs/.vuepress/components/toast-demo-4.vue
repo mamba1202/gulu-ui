2f2d2d2cee7189a8abaa2e344ebd799bf23ce572
mamba <1612786059@qq.com>
2019-05-24 11:35:12 +0800

    添加自动或手动关闭

---
+++ docs/.vuepress/components/toast-demo-4.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="padding-top: 16px;">
+    <p>
+      <strong>预览</strong>
+    </p>
+
+    <div>
+      <g-button @click="onClickButton">上方弹出</g-button>
+    </div>
+  </div>
+</template>
+<style>
+  .gulu-toast {
+    z-index: 30 !important;
+  }
+</style>
+<style scoped>
+  * {
+    box-sizing: border-box;
+  }
+</style>
+<script>
+  import plugin from '../../../src/plugin'
+  import GButton from '../../../src/button'
+  import Vue from 'vue'
+  Vue.use(plugin)
+  export default {
+    components: {GButton},
+    methods: {
+      onClickButton () {
+        this.$toast('我不会自动关闭', {
+          autoClose: false
+        })
+      }
+    }
+  }
+</script>
