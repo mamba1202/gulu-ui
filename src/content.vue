565a52410d1bf1f43db5bb01891f69903ae66bc8
mamba <1612786059@qq.com>
2019-05-24 11:40:15 +0800

    修改class-name

--- src/content.vue
+++ src/content.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div class="cont">
     <slot></slot>
   </div>
 </template>
@@ -9,7 +9,7 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.content{
+.cont{
     flex-grow: 1;
 }
 </style>
