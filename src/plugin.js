c86e5adf7bbe89c454dcb8d32723e1ad8420486e
mamba <1612786059@qq.com>
2019-05-25 11:51:12 +0800

    导出文件修改

--- src/plugin.js
+++ src/plugin.js
@@ -1,53 +1,56 @@
-import Toast from './toast.vue'
-// export default {
-//     install(Vue, options) {
-//         Vue.prototype.$toast = function (message,toastOptions) {
-//             let Constructor = Vue.extend(Toast)
-//             let toast = new Constructor({
-//                 propsData: toastOptions
-//                     //closeButton: toastOptions.closeButton
-
-//             }) //实例
-//             toast.$slots.default = [message]
-//             toast.$mount()
-//             document.body.appendChild(toast.$el)
-//         }
-//     }
-// }
+import Toast from './toast'
 
 
 let currentToast
+
 export default {
-    install(Vue, options) {
-        Vue.prototype.$toast = function (message, toastOptions) {
-            if(currentToast){
-                currentToast.close()
-            }
-            currentToast = creatToast({ 
-                Vue,
-                 message,
-                  propsData: toastOptions,
-                  onClose:()=>{
-                      currentToast = null
-                  }
-                 })
+  install (Vue, options) {
+    Vue.prototype.$toast = function (message, toastOptions) {
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({
+        Vue,
+        message,
+        propsData: toastOptions,
+        onClose: () => {
+          currentToast = null
         }
+      })
     }
+  }
 }
 
 
 
 
-/*helper*/ 
-function creatToast({ Vue, message, propsData,onClose }) {
-    let Constructor = Vue.extend(Toast)
-    let toast = new Constructor({
-        propsData: propsData
 
-    })
-    toast.$slots.default = [message]
-    toast.$mount()
-    toast.$on('close',onClose)
-    document.body.appendChild(toast.$el)
-    return toast
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* helpers */
+function createToast ({Vue, message, propsData, onClose}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({propsData})
+  toast.$slots.default = [message]
+  toast.$mount()
+  toast.$on('close', onClose)
+  document.body.appendChild(toast.$el)
+  return toast
 }
